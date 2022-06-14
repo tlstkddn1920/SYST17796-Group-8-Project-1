ea6275efe0a5e3bd359174756677fe18b9b48b6c
Adam Shin <73809850+tlstkddn1920@users.noreply.github.com>
2022-06-14 17:23:36 -0400

    Card.java
    Updated author

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -9,6 +9,9 @@ package ca.sheridancollege.project;
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  * @author The Fisher
+ * @Adam Shin
+ * @author
+ * @author
  */
 public abstract class Card {
     //default modifier for child classes
