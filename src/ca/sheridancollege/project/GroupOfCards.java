bdd90b59e76cc87b0142d4a09f868c01be5b6881
Adam Shin <73809850+tlstkddn1920@users.noreply.github.com>
2022-06-14 17:34:32 -0400

    Update GroupOfCards.java

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -12,8 +12,8 @@ import java.util.Collections;
  * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
  * once. The group of cards has a maximum size attribute which is flexible for reuse.
  *
- * @author dancye
- * @author Paul Bonenfant
+ * @author The Fisher
+ * @author Adam Shin
  * @author Prem Parashar
  */
 public class GroupOfCards {
