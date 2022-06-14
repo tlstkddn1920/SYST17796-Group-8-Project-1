b4849e5139f13854f30fb8e90b81edee7bd93379
garnetmi <85130542+garnetmi@users.noreply.github.com>
2022-06-14 17:47:52 -0400

    Update GroupOfCards.java

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 /**
  * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
  * once. The group of cards has a maximum size attribute which is flexible for reuse.
- *
+ * Update???????
  * @author The Fisher
  * @author Adam Shin
  * @author Micah Garnett
