afcd3e2f53d872cae42f2faae62a84c31ee2bb72
Adam Shin <73809850+tlstkddn1920@users.noreply.github.com>
2022-06-14 17:33:41 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -9,7 +9,7 @@ package ca.sheridancollege.project;
  * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
  * game. Students wishing to add to the code should remember to add themselves as a modifier.
  * @author The Fisher
- * @Adam Shin
+ * @author Adam Shin
  * @author
  * @author
  */
